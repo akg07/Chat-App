2f16409c1dda202d77391eb4f06768f91b903497
Ayush <ayush3032@gmail.com>
2024-07-02 00:58:12 +0530

    add all chat routes and its controller.

--- backend/app.js
+++ backend/app.js
@@ -2,27 +2,29 @@ const express = require("express");
 const dotenv = require("dotenv");
 const conenctDB = require("./config/db");
 const app = express();
-const colors = require('colors');
-const { notFound, errorHandler } = require('./middleware/error-handler');
+const colors = require("colors");
+const { notFound, errorHandler } = require("./middleware/error-handler");
 
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 // const cors = require('cors');
-
 
 // app.use(cors());
 dotenv.config();
 conenctDB();
 app.use(express.json()); // accpet data in json
 
+require("./models/messageModel");
 
 app.get("/", (req, res) => {
   res.send("home ");
 });
 
-app.use("/api/user", userRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => {
