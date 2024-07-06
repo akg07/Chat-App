05b9040d2d315ada3d343083572324e6518f22f4
Ayush <ayush3032@gmail.com>
2024-07-06 14:41:38 +0530

    add prod env

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const { notFound, errorHandler } = require("./middleware/error-handler");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const path = require("path");
 // const cors = require('cors');
 
 // app.use(cors());
@@ -17,13 +18,25 @@ app.use(express.json()); // accpet data in json
 
 require("./models/messageModel");
 
-app.get("/", (req, res) => {
-  res.send("home ");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+///////////////// Deployment
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static(path.join(__dirname1, '/frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  })
+
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running Successfully.");
+  });
+}
+///////////////// Deployment
 
 app.use(notFound);
 app.use(errorHandler);
